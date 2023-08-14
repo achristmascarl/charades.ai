c2e999610c4b8c1fbd28283baa17d38f5044305b
achristmascarl <carl.z.liu@gmail.com>
2023-08-14 02:36:22 -0400

    get script almost working

--- scripts/generateCharades.mjs
+++ scripts/generateCharades.mjs
@@ -8,9 +8,8 @@ import fs from "fs";
 import { v4 as uuidv4 } from "uuid";
 import { wordList } from "../src/utils.js";
 import { MongoClient } from "mongodb";
-// import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-// const REGION = "us-east-2";
-// const s3Client = new S3Client({ region: REGION });
+import { S3 } from "@aws-sdk/client-s3";
+const s3 = new S3({region: "us-east-2"});
 
 import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
@@ -55,16 +54,63 @@ console.log(id);
 const promptIndex = Math.floor(Math.random() * wordList.length);
 const prompt = wordList[promptIndex];
 console.log(prompt);
-const response = await openai.createImage({
-  prompt: prompt,
-  n: 1,
-  size: "256x256",
-});
+let response;
+try {
+  response = await openai.createImage({
+    prompt: prompt,
+    n: 1,
+    size: "256x256",
+  });
+} catch (err) {
+  console.log(err);
+  try {
+    response = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: "256x256",
+    });
+  } catch (err) {
+    console.log(err);
+    throw new Error(err);
+  }
+}
 const imageUrl = response.data.data[0].url;
 console.log(imageUrl);
 const file = fs.createWriteStream(`tmp/${id}.jpg`);
 https.get(imageUrl, function(response) {
   response.pipe(file);
+});
+
+await file.on("close", async function() {
+  console.log("File written!");
+  file.close();
+  const blob = fs.readFileSync(`tmp/${id}.jpg`);
+  console.log(blob);
+  const params = {
+    Bucket: "charades.ai",
+    Key: `images/${id}.jpg`,
+    Body: blob,
+  };
+  
+  try {
+    const response = await s3.putObject(params);
+    console.log(response);
+    try {
+      charades.insertOne({
+        _id: id,
+        isoDate: new Date(generationInfo[0].isoDateString),
+        isoDateId: generationInfo[0].isoDateId,
+        charadeIndex: generationInfo[0].charadeIndex,
+        answer: prompt,
+      });
+    } catch (err) {
+      console.log(err);
+      throw new Error(err);
+    }
+  } catch (err) {
+    console.log(err);
+    throw new Error(err);
+  }
 });
 
 // }
