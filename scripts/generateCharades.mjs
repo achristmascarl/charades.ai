a61434f0cb422d376d836295ad221217b0dde558
achristmascarl <carl.z.liu@gmail.com>
2023-08-14 21:45:42 -0400

    fix generation script

--- scripts/generateCharades.mjs
+++ scripts/generateCharades.mjs
@@ -14,6 +14,7 @@ const s3 = new S3({region: "us-east-2"});
 import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
+  temperature: 1.5,
 });
 const openai = new OpenAIApi(configuration);
 
@@ -31,7 +32,7 @@ await results.forEach((result) => {
 });
 console.log(recentCharades);
 let generationInfo = [];
-for (let i = 1; i < 8; i++) {
+for (let i = 1; i < 6; i++) {
   const lastDate = new Date(recentCharades[0].isoDateId);
   console.log(lastDate);
   lastDate.setUTCHours(lastDate.getUTCHours() + (i * 24));
@@ -48,7 +49,7 @@ for (let i = 1; i < 8; i++) {
 console.log(generationInfo);
 
 console.log(process.cwd());
-for (let i = 0; i < generationInfo.length; i++) {
+for (let i = 0; i < generationInfo.length ; i++) {
   const id = uuidv4();
   console.log(id);
   const promptIndex = Math.floor(Math.random() * answerList.length);
@@ -58,54 +59,58 @@ for (let i = 0; i < generationInfo.length; i++) {
   try {
     response = await openai.createImage({
       prompt: prompt,
-      n: 1,
+      n: 5,
       size: "256x256",
     });
   } catch (err) {
     console.log(err);
-    try {
-      response = await openai.createImage({
-        prompt: prompt,
-        n: 1,
-        size: "256x256",
-      });
-    } catch (err) {
-      console.log(err);
-      throw new Error(err);
-    }
+    throw new Error(err);
   }
-  const imageUrl = response.data.data[0].url;
-  console.log(imageUrl);
-  const file = fs.createWriteStream(`tmp/${id}.jpg`);
-  https.get(imageUrl, function(response) {
-    response.pipe(file);
-  });
-
-  await file.on("close", async function() {
-    console.log("File written!");
-    file.close();
-    const blob = fs.readFileSync(`tmp/${id}.jpg`);
-    console.log(blob);
-    const params = {
-      Bucket: "charades.ai",
-      Key: `images/${id}.jpg`,
-      Body: blob,
-    };
-    
-    try {
-      const response = await s3.putObject(params);
-      console.log(response);
-      await charades.insertOne({
-        _id: id,
-        isoDate: new Date(generationInfo[i].isoDateString),
-        isoDateId: generationInfo[i].isoDateId,
-        charadeIndex: generationInfo[i].charadeIndex,
-        answer: prompt,
-      });
-    } catch (err) {
-      console.log(err);
-      throw new Error(err);
+  let uploadPromises = [];
+  try {
+    for (let j = 0; j < 5; j++) {
+      uploadPromises.push(new Promise((resolve) => {
+        try {
+          const imageUrl = response.data.data[j].url;
+          console.log(imageUrl);
+          const file = fs.createWriteStream(`tmp/${id}${j === 0 ? "" : `-${j}`}.jpg`);
+          https.get(imageUrl, function(response) {
+            response.pipe(file);
+          });
+        
+          file.on("close", async () => {
+            console.log(`File ${j + 1} written for prompt ${i}!`);
+            const blob = fs.readFileSync(`tmp/${id}${j === 0 ? "" : `-${j}`}.jpg`);
+            file.close();
+            console.log(blob);
+            const params = {
+              Bucket: "charades.ai",
+              Key: `images/${id}${j === 0 ? "" : `-${j}`}.jpg`,
+              Body: blob,
+            };
+            
+            let s3response = await s3.putObject(params);
+            resolve(s3response);
+          });
+        } catch (err) {
+          console.log(err);
+          throw new Error(err);
+        }
+      }));
     }
+  } catch (err) {
+    console.log(err);
+    throw new Error(err);
+  }
+  await Promise.all(uploadPromises).then(async () => {
+    await charades.insertOne({
+      _id: id,
+      isoDate: new Date(generationInfo[i].isoDateString),
+      isoDateId: generationInfo[i].isoDateId,
+      charadeIndex: generationInfo[i].charadeIndex,
+      answer: prompt,
+    });
+    console.log(`db entry created for prompt ${i}!`);
   });
 }
 console.log(`finished generating ${generationInfo.length} charades`);
