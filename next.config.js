2b2b7e2377197affff5db3f08dcc6aec2775f01c
achristmascarl <carl.z.liu@gmail.com>
2023-08-30 17:59:58 -0400

    nit: eslint for configs

--- next.config.js
+++ next.config.js
@@ -2,19 +2,17 @@
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
-})
+});
 const nextConfig = {
   publicRuntimeConfig: {
-    NODE_ENV: "production"
+    NODE_ENV: "production",
   },
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [
-      "s3.us-east-2.amazonaws.com",
-    ],
+    domains: ["s3.us-east-2.amazonaws.com"],
     unoptimized: true,
   },
-}
+};
 
 module.exports = withBundleAnalyzer(nextConfig);
