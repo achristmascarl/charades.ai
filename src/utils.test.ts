65cd3a23ed3ca736d363d779abcb1cbd4847ae7a
achristmascarl <carl.z.liu@gmail.com>
2023-08-30 16:54:36 -0400

    fix: update tests to ts

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,7 +1,8 @@
 import { answerList, wordList } from "./utils";
-import { MongoClient } from "mongodb";
+import { MongoClient, Db, Collection } from "mongodb";
 import { ListObjectsV2Command, S3Client } from "@aws-sdk/client-s3";
 import { test, expect, describe, beforeAll, afterAll } from "@jest/globals";
+import Charade from "./models/charade";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -22,17 +23,17 @@ test("word list is all 5-letter words", () => {
 });
 
 describe("today's and next 6 rounds of charades valid", () => {
-  let url;
-  let client;
-  let database;
-  let charades;
+  let client: MongoClient;
+  let database: Db;
+  let charades: Collection;
   let s3client;
-  let s3objects = [];
+  let s3objects: string[] = [];
 
   beforeAll(async () => {
-    url = process.env.MONGO_URL;
-    expect(url).toBeTruthy();
-    client = await MongoClient.connect(url);
+    if (process.env.MONGO_URL === undefined) throw new Error("MONGO_URL not defined");
+    expect(process.env.MONGO_URL).toBeTruthy();
+    client = new MongoClient(process.env.MONGO_URL);
+    await client.connect();
     expect(client).toBeTruthy();
     expect(client).not.toBeNull();
     expect(client).not.toBeUndefined();
@@ -55,13 +56,13 @@ describe("today's and next 6 rounds of charades valid", () => {
       let isTruncated = true;
       while (isTruncated) {
         const { Contents, IsTruncated, NextContinuationToken } = await s3client.send(command);
-        Contents.forEach((c) => s3objects.push(c.Key));
-        isTruncated = IsTruncated;
+        Contents?.forEach((c) => c.Key && s3objects.push(c.Key));
+        isTruncated = IsTruncated || false;
         command.input.ContinuationToken = NextContinuationToken;
       }
-    } catch (err) {
+    } catch (err: unknown) {
       console.log(err);
-      throw new Error(err);
+      throw new Error(err as string);
     }
     expect(s3objects).toBeTruthy();
     expect(s3objects.length).toBeGreaterThan(0);
@@ -82,8 +83,11 @@ describe("today's and next 6 rounds of charades valid", () => {
   }
   dateIdQueries.forEach(async (dateIdQuery) =>
     test(`charades ${dateIdQuery.isoDateId} valid`, async () => {
-      const charade = await charades.findOne(dateIdQuery);
-      const charadeId = charade._id.toString();
+      const document = await charades.findOne(dateIdQuery);
+      const charade = new Charade(document?.charadeIndex, document?.answer, document?.isoDate, document?.isoDateId)
+      const charadeId = document?._id.toString();
+      expect(charadeId).toBeTruthy();
+      expect(charadeId?.length).toBeGreaterThan(0);
       expect(charade).toBeTruthy();
       expect(charade.charadeIndex).toBeTruthy();
       expect(charade.charadeIndex.length).toBeGreaterThan(0);
