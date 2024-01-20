f70c70ca14d2af111a1fd31a63dcdbb19aa5111f
achristmascarl <carl.z.liu@gmail.com>
2024-01-20 18:19:02 -0500

    try fix tests

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,8 @@ const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/", "src"],
   modulePaths: ["<rootDir>/src/"],
   setupFiles: ["<rootDir>/jest-shim.js"],
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/',
+  ],
 };
 module.exports = createJestConfig(customJestConfig);
