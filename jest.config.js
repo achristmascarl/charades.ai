0a7ae5666e7e72644aab0bf96f55f5bd9a2a9ded
achristmascarl <carl.z.liu@gmail.com>
2024-01-20 18:52:11 -0500

    undo a bunch of stuff

--- jest.config.js
+++ jest.config.js
@@ -6,8 +6,5 @@ const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/", "src"],
   modulePaths: ["<rootDir>/src/"],
   setupFiles: ["<rootDir>/jest-shim.js"],
-  transformIgnorePatterns: [
-    '<rootDir>/node_modules/',
-  ],
 };
 module.exports = createJestConfig(customJestConfig);
